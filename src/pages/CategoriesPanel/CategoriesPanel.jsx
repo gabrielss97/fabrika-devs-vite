dab22f24e7c3cb8dfcd22aa4044e4d2faf05290e
yagox <yago.ramiresx@gmail.com>
2022-09-01 23:17:56 -0300

    feat(Categories): categories styles

--- src/pages/CategoriesPanel/CategoriesPanel.jsx
+++ src/pages/CategoriesPanel/CategoriesPanel.jsx
@@ -1,3 +1,7 @@
+// Icons
+import { CgRemove } from 'react-icons/cg';
+import { HiOutlinePencilAlt } from 'react-icons/hi';
+
 // Hooks
 import { useState } from 'react';
 import { useInsertDocument } from '../../hooks/useInsertDocument';
@@ -33,27 +37,38 @@ const CategoriesPanel = () => {
     <div>
       {categories && (
         <>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor='newCategory'>
-              <span>Nova categoria:</span>
+          <form
+            onSubmit={handleSubmit}
+            className='flex gap-4 p-4 md:max-w-5xl mx-auto mt-16  items-center '>
+            <label htmlFor='newCategory' className='flex w-full items-center'>
+              <span className='w-1/4'>Nova categoria:</span>
               <input
                 type='text'
                 value={newCategory || ''}
                 onChange={(e) => setNewCategory(e.target.value)}
+                className='form-input bg-cLtGray w-full'
               />
             </label>
-            <input type='submit' value='Adicionar' />
+            <input type='submit' value='Adicionar' className='btn' />
           </form>
-          <div>
+          <div className='flex flex-col gap-4 p-4 md:max-w-5xl mx-auto mt-16'>
             {categories.length > 0 &&
               categories
                 .sort((a, b) => a.id - b.id)
                 .map((category) => (
-                  <div key={category.id} className='flex gap-4'>
-                    <p>{category.name}</p>
-                    <div className='flex gap-4'>
-                      <button>edit</button>
-                      <button>delete</button>
+                  <div
+                    key={category.id}
+                    className='flex w-full bg-cLtGray p-4 rounded-md justify-between'>
+                    <p className='text-xl font-bold text-cBlue'>
+                      {category.name}
+                    </p>
+                    <div className='flex gap-4 text-xl'>
+                      <button type='button'>
+                        <HiOutlinePencilAlt className='text-cGreen' />
+                      </button>
+                      <button type='button'>
+                        <CgRemove className='text-cRed' />
+                      </button>
                     </div>
                   </div>
                 ))}
