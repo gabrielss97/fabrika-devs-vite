8ab35f8ff46a6cb8c6b1400eefe41a73fc396d47
yagox <yago.ramiresx@gmail.com>
2022-09-06 15:05:46 -0300

    fix(App): verificacao e redirect nas rotas

--- src/App.jsx
+++ src/App.jsx
@@ -71,18 +71,64 @@ function App() {
           />
           {user && !admin && (
             <>
-              <Route path='/' element={<Content />} />
-              <Route path='/profile' element={<Profile user={user} />} />
+              <Route
+                path='/'
+                element={
+                  user && !admin ? <Content /> : <Navigate to='/login' />
+                }
+              />
+              <Route
+                path='/profile'
+                element={
+                  user && !admin ? (
+                    <Profile user={user} />
+                  ) : (
+                    <Navigate to='/login' />
+                  )
+                }
+              />
             </>
           )}
           {user && admin && (
             <>
-              <Route path='/' element={<Panel user={user} />} />
-              <Route path='/profile' element={<Profile user={user} />} />
-              <Route path='/admins' element={<Admins />} />
-              <Route path='/users' element={<Users />} />
-              <Route path='/categories' element={<Categories />} />
-              <Route path='/videos' element={<Videos />} />
+              <Route
+                path='/'
+                element={
+                  user && admin ? (
+                    <Panel user={user} />
+                  ) : (
+                    <Navigate to='/login' />
+                  )
+                }
+              />
+              <Route
+                path='/profile'
+                element={
+                  user && admin ? (
+                    <Profile user={user} />
+                  ) : (
+                    <Navigate to='/login' />
+                  )
+                }
+              />
+              <Route
+                path='/admins'
+                element={user && admin ? <Admins /> : <Navigate to='/login' />}
+              />
+              <Route
+                path='/users'
+                element={user && admin ? <Users /> : <Navigate to='/login' />}
+              />
+              <Route
+                path='/categories'
+                element={
+                  user && admin ? <Categories /> : <Navigate to='/login' />
+                }
+              />
+              <Route
+                path='/videos'
+                element={user && admin ? <Videos /> : <Navigate to='/login' />}
+              />
             </>
           )}
           <Route path='/*' element={<Navigate to='/404' />} />
